949abe4175dc895785cbc48269579fc451041d7a
René Köcher <info@bitspin.org>
2015-07-03 18:22:13 +0200

    Don't use normal mp() on device (it causes crashes..).

--- app/helpers/motion_print.rb
+++ app/helpers/motion_print.rb
@@ -1,13 +1,46 @@
 # Promotion ditched their logger in favor of
 # MotionPrint.. add some helpers to ease life
-def mp_l( message )
-  mp "[INFO] #{message}", force_color: :green
+module MotionPrint
+  class << self
+    def simulator?
+      @simulator ||= !(UIDevice.currentDevice.model =~ /simulator/i).nil?
+    end
+
+    def use_nslog
+      @use_nslog = true
+    end
+
+    def use_nslog?
+      !@use_nslog.nil?
+    end
+  end
 end
 
-def mp_d( message )
-  mp "[DEBUG] #{message}", force_color: :yellow
+module Kernel
+  def mp_l( message )
+    if MotionPrint.use_nslog?
+      NSLog( "[INFO] #{message}" )
+    else
+      return unless MotionPrint.simulator?
+    end
+    mp "[INFO] #{message}", force_color: :green
+  end
+
+  def mp_d( message )
+    if MotionPrint.use_nslog?
+      NSLog( "[DEBUG] #{message}" )
+    else
+      return unless MotionPrint.simulator?
+    end
+    mp "[DEBUG] #{message}", force_color: :yellow
+  end
+
+  def mp_e( message )
+    if MotionPrint.simulator? and ! MotionPrint.use_nslog
+      mp "[ERROR] #{message}", force_color: :red
+    else
+      NSLog( "[ERROR] #{message}")
+    end
+  end
 end
 
-def mp_e( message )
-  mp "[ERROR] #{message}", force_color: :red
-end
