b284459cd12476b1d3261ee8f6af221de8ef4a6b
René Köcher <info@bitspin.org>
2015-08-15 13:56:17 +0200

    Extend Hash to allow conversion of keys to Symbols.

---
+++ lib/hash+sybolicate.rb
@@ -0,0 +1,21 @@
+class Hash
+  # Return a new hash with all keys replaced by +Symbol+s.
+  # This operation is performed recursively on all elements in the +Hash+
+  # and will also iterate over +Array+ values to check for nested +Hash+es.
+  #
+  # @param [Hash] hash The hash to symbolicate
+  # @return [Hash] A new hash with only +Symbol+s used as keys.
+  def self.symbolicate( hash )
+    hash.each_with_object({}) do |(k, v), g|
+      g[k.to_sym] = (Hash === v) ? Hash.symbolicate(v)
+                                 : ((Array === v) ? v.map { |e| (Hash === e) ? Hash.symbolicate(e) : e }
+                                                  : v)
+    end
+  end
+
+  # See +Hash#symbolicate
+  def symbolicate
+    Hash.symbolicate(self)
+  end
+end
+
