160700da49300500f149e39d74dc99fe2a859849
René Köcher <info@bitspin.org>
2015-08-17 20:33:10 +0200

    Extension for +private_module_function+'s. Not sure I like this implementation 100%, it might change in the future.

---
+++ lib/module+scope.rb
@@ -0,0 +1,11 @@
+# Declare +names+ to be private module functions.
+# @param [Array] *names A list of names that should be converted to
+#   private module functions.
+# @todo Not sure I like how this polutes the global scope..
+def private_module_function( *names )
+    names.each do |name|
+      module_function name
+      private_class_method name
+    end
+end
+
