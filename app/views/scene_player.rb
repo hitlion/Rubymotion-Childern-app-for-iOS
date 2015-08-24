8d7bd571a9f15c1380a9406b49ff81db33060a3a
René Köcher <info@bitspin.org>
2015-08-24 20:23:25 +0200

    Code cleanup, add swipe event recognition.

--- app/views/scene_player.rb
+++ app/views/scene_player.rb
@@ -1,17 +1,33 @@
 class ScenePlayer < SKView
   def initWithFrame( frame )
     super( frame ).tap do
-      on(:tap) do |_, event|
+      on(:tap, fingers_required: 1) do |_, event|
         return if scene.nil?
 
-        point = scene.convertPointFromView(event.location)
-        object = scene.nodeAtPoint(point)
-
+        object = object_at_location(event.location)
         unless object.nil?
           JavaScript::Runtime.send_event(object.name, :on_click)
         end
       end
+
+      on(:swipe) do |_, event|
+        return if scene.nil?
+
+        object = object_at_location(event.location)
+        unless object.nil?
+          JavaScript::Runtime.send_event(object.name, :on_swipe)
+        end
+      end
+
+      on(:pan) { |_, _| rmq(:logger).map { |l| l.show } }
     end
+  end
+
+  private
+
+  def object_at_location( location )
+    point = scene.convertPointFromView(location)
+    scene.nodeAtPoint(point)
   end
 end
 
