773affc67f1129cc73cc5918455e8afd6ae43cd0
René Köcher <info@bitspin.org>
2015-08-26 18:28:21 +0200

    Add YARD comments.

--- app/controllers/auto_rotating_navigation_controller.rb
+++ app/controllers/auto_rotating_navigation_controller.rb
@@ -1,14 +1,17 @@
 # UINavigationController with proper support for forced view orientations
 # Thanks go out to: http://stackoverflow.com/a/14402965
 class AutoRotatingNavigationController < PM::NavigationController
+  # @private
   def shouldAutorotate
     self.topViewController.shouldAutorotate
   end
 
+  # @private
   def supportedInterfaceOrientations
     self.topViewController.supportedInterfaceOrientations
   end
 
+  # @private
   def preferredInterfaceOrientationForPresentation
     self.topViewController.preferredInterfaceOrientationForPresentation
   end
