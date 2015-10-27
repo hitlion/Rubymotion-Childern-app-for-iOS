00df5314350a0ff3ee719d2039f4b3704b7b49cc
BitspinProductions <info@bitspin.org>
2015-10-27 10:43:58 +0000

    Fix resource cleanup when creating / deleting SKView.

--- app/screens/kids_screen.rb
+++ app/screens/kids_screen.rb
@@ -3,11 +3,16 @@ class KidsScreen < PM::Screen
   attr_accessor :story_list
 
   def will_appear
-    player = SKView.alloc.initWithFrame(self.view.bounds)
-    rmq(self.view).append(player)
-    scene = KidsScene.alloc.initWithSize(self.view.size)
+    @player = SKView.alloc.initWithFrame(view.bounds)
+    rmq(view).append(@player)
+    scene = KidsScene.alloc.initWithSize(view.size)
     scene.story_list = @story_list
-    player.presentScene(scene)
+    @player.presentScene(scene)
   end
 
+  def will_disappear
+    @player.presentScene(nil)
+    rmq(@player).remove
+    @player = nil
+  end
 end
