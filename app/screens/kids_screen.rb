f2ce94b6c10d8c066d1186195dbf610f0b5d5808
Robert Schertling <robertschertling@gmx.de>
2015-10-27 12:26:27 +0100

    * changed story_bundle from reader to accessor in story_bundle.rb
    * used StoryBundle.bundles to collect the story in kids_screen.rb and kids_scene

--- app/screens/kids_screen.rb
+++ app/screens/kids_screen.rb
@@ -1,12 +1,9 @@
 class KidsScreen < PM::Screen
 
-  attr_accessor :story_list
-
   def will_appear
     @player = SKView.alloc.initWithFrame(view.bounds)
     rmq(view).append(@player)
     scene = KidsScene.alloc.initWithSize(view.size)
-    scene.story_list = @story_list
     @player.presentScene(scene)
   end
 
