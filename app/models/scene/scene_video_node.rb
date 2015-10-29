c640fc2db03722f21db03c0a5f9b7387b5a45687
René Köcher <info@bitspin.org>
2015-10-29 20:33:43 +0100

    Fix a crash when trying to create a VideoNode.

--- app/models/scene/scene_video_node.rb
+++ app/models/scene/scene_video_node.rb
@@ -4,32 +4,67 @@ module Scene
 
     attr_reader :av_player
 
-    # Create a new VideoNode bound to +story_object+
-    #
-    # @param [StoryBundle] bundle The {StoryBundle} containing +story_object+.
-    # @param [Scene::Object] story_object The object definition.
-    def self.create( bundle, story_object )
-      # pre-setup, fetch the video asset and extract it's size
-      asset_url  = bundle.asset_path(story_object.content).to_file_url
-      asset      = load_video_asset(asset_url)
-      video_size = fetch_video_size(asset)
+    class << self
+      # Create a new VideoNode bound to +story_object+
+      #
+      # @param [StoryBundle] bundle The {StoryBundle} containing +story_object+.
+      # @param [Scene::Object] story_object The object definition.
+      def create( bundle, story_object )
+        # pre-setup, fetch the video asset and extract it's size
+        asset_url  = bundle.asset_path(story_object.content).to_file_url
+        asset      = load_video_asset(asset_url)
+        video_size = fetch_video_size(asset)
 
-      av_item   = AVPlayerItem.playerItemWithAsset(asset)
-      av_player = AVPlayer.playerWithPlayerItem(av_item)
+        av_item   = AVPlayerItem.playerItemWithAsset(asset)
+        av_player = AVPlayer.playerWithPlayerItem(av_item)
 
-      VideoNode.alloc.initWithAVPlayer(av_player).tap do |node|
-        node.instance_eval do
-          self.name      = story_object.path
-          self.size      = calculate_node_size(story_object.size,
-                                               video_size.width / video_size.height,
-                                               story_object.resize)
-          self.position  = calculate_node_position(story_object.position,
-                                                   self.size)
-          self.zPosition = story_object.layer
-          self.alpha     = 1.0 - story_object.transparency
+        VideoNode.alloc.initWithAVPlayer(av_player).tap do |node|
+          node.instance_eval do
+            self.name      = story_object.path
+            self.size      = calculate_node_size(story_object.size,
+                                                 video_size.width / video_size.height,
+                                                 story_object.resize)
+            self.position  = calculate_node_position(story_object.position,
+                                                     self.size)
+            self.zPosition = story_object.layer
+            self.alpha     = 1.0 - story_object.transparency
 
-          # Setup playback notifications (why can't this be done via delegation!?)
-          @av_player     = av_player
+            # Setup playback notifications (why can't this be done via delegation!?)
+            @av_player     = av_player
+          end
+        end
+      end
+
+      # Load the video at +asset_url+ and return a matching +AVAsset+.
+      # If the video ist not available a placeholder will be returned instead.
+      #
+      # @param [String] asset_url The file system URL to the asset.
+      # @return [AVAsset] The initialized asset instance.
+      def load_video_asset( asset_url )
+        unless File.exists? asset_url.fileSystemRepresentation
+          lp "Video asset '#{asset_url.fileSystemRepresentation}' is missing.",
+             force_color: :cyan
+
+          asset_url = Dir.resource('placeholder/file_warning.m4v').to_file_url
+        end
+
+        AVURLAsset.URLAssetWithURL(asset_url, options: nil)
+      end
+
+      # Fetch the frame size of the first video track inside +asset+.
+      # 
+      # @param [String] asset The video asset to extract the size from.
+      # @return [CGSize] The videos frame size (naturalSize) or [1.0,1.0]
+      #   in case of errors.
+      def fetch_video_size( asset )
+        media_type = AVMediaTypeVideo
+        track_info = asset.tracksWithMediaType(media_type).first
+
+        if track_info.nil?
+          lp 'Video asset contains no video tracks!', force_color: :red
+          CGSize.new(1.0, 1.0)
+        else
+          track_info.naturalSize
         end
       end
     end
@@ -56,39 +91,6 @@ module Scene
 
 
     private
-
-    # Load the video at +asset_url+ and return a matching +AVAsset+.
-    # If the video ist not available a placeholder will be returned instead.
-    #
-    # @param [String] asset_url The file system URL to the asset.
-    # @return [AVAsset] The initialized asset instance.
-    def load_video_asset( asset_url )
-      unless File.exists? asset_url.fileSystemRepresentation
-        lp "Video asset '#{asset_url.fileSystemRepresentation}' is missing.",
-           force_color: :cyan
-
-        asset_url = Dir.resource('placeholder/file_warning.m4v').to_file_url
-      end
-
-      AVURLAsset.URLAssetWithURL(asset_url, options: nil)
-    end
-
-    # Fetch the frame size of the first video track inside +asset+.
-    # 
-    # @param [String] asset The video asset to extract the size from.
-    # @return [CGSize] The videos frame size (naturalSize) or [1.0,1.0]
-    #   in case of errors.
-    def fetch_video_size( asset )
-      media_type = AVMediaTypeVideo
-      track_info = asset.tracksWithMediaType(media_type).first
-
-      if track_info.nil?
-        lp 'Video asset contains no video tracks!', force_color: :red
-        CGSize.new(1.0, 1.0)
-      else
-        track_info.naturalSize
-      end
-    end
 
     # Register for playback notifications with the global NotificationCenter
     def register_notifications
