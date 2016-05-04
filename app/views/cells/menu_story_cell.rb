7c6e1baaec2681872d22e254256d033852cde2d2
Robert Schertling <Robertschertling@googlemail.com>
2016-05-04 15:58:59 +0200

    # fixed Problems in MenuStoryCell

--- app/views/cells/menu_story_cell.rb
+++ app/views/cells/menu_story_cell.rb
@@ -8,60 +8,49 @@ class MenuStoryCell < UICollectionViewCell
 
       self.backgroundColor = rmq.color.babbo_orange
 
-      @background_image = UIImageView.alloc.initWithFrame(self.bounds)
+      view = UIButton.alloc.initWithFrame(self.bounds)
+      view.addTarget(self, action: "cell_pressed:", forControlEvents: UIControlEventTouchUpInside)
+      view.backgroundColor = UIColor.clearColor
+
+      @background_image = UIImageView.alloc.initWithFrame(view.bounds)
       @background_image.backgroundColor = rmq.color.blue
-      @background_image.off
-      @background_image.on(:tap) do
-        right_button_pressed(@right_button)
-      end
+      view.addSubview(@background_image) unless @background_image.nil?
 
-      append(@background_image) unless @background_image.nil?
-
-      layer = UIImageView.alloc.initWithFrame(CGRectMake(0, 0, 0.75 * self.frame.size.width, 0.33 * self.frame.size.height ))
+      layer = UIImageView.alloc.initWithFrame(CGRectMake(0, 0, 0.75 * view.frame.size.width, 0.33 * view.frame.size.height ))
       layer.image = rmq.image.resource("cells/cell_layer.png")
-      append(layer) unless layer.nil?
+      view.addSubview(layer) unless layer.nil?
 
       @label = UILabel.alloc.initWithFrame(CGRectMake(0, 0, layer.frame.size.width, 0.5 * layer.frame.size.height))
       @label.text = 'no text found'
       @label.textColor = rmq.color.black
       device.ipad? ? @label.font = rmq.font.pad_large : @label.font = rmq.font.phone_large_bold
-
       @label.textAlignment = UITextAlignmentCenter
-      append(@label) unless @label.nil?
+      view.addSubview(@label) unless @label.nil?
 
       @left_button = UIButton.alloc.initWithFrame(CGRectMake(0.025 * layer.frame.size.width,0.6 * layer.frame.size.height,
                                                             0.425 * layer.frame.size.width, 0.35 * layer.frame.size.height))
       @left_button.setBackgroundImage(rmq.image.resource("button_grey.png"), forState:UIControlStateNormal)
       @left_button.setTitle("Mehr", forState: UIControlStateNormal)
+      @left_button.addTarget(self, action: "left_button_pressed:", forControlEvents: UIControlEventTouchUpInside)
       device.ipad? ? @left_button.font = rmq.font.pad_medium : @left_button.font = rmq.font.phone_medium
-
-      @left_button.off
-      @left_button.on(:tap) do
-        left_button_pressed(@left_button)
-      end
-      append(@left_button) unless @left_button.nil?
+      view.addSubview(@left_button) unless @left_button.nil?
 
       @right_button = UIButton.alloc.initWithFrame(CGRectMake(0.50 * layer.frame.size.width,0.6 * layer.frame.size.height,
                                                              0.425 * layer.frame.size.width, 0.35 * layer.frame.size.height))
       @right_button.setBackgroundImage(rmq.image.resource("button_orange.png"), forState:UIControlStateNormal)
       @right_button.setTitle("Öffnen", forState: UIControlStateNormal)
-
+      @right_button.addTarget(self, action: "right_button_pressed:", forControlEvents: UIControlEventTouchUpInside)
       device.ipad? ? @right_button.font = rmq.font.pad_medium : @right_button.font = rmq.font.phone_medium
-      @right_button.off
-      @right_button.on(:tap) do
-        right_button_pressed(@right_button)
-      end
-      append(@right_button) unless @right_button.nil?
+      view.addSubview(@right_button) unless @right_button.nil?
 
-
-
-      @selected_story_marker = UIImageView.alloc.initWithFrame(CGRectMake(CGRectGetMidX(self.bounds)- 0.05 *  self.frame.size.width,
-                                                                          self.frame.size.height - 0.05 * self.frame.size.width,
-                                                                          0.1 * self.frame.size.width, 0.05 * self.frame.size.width))
+      @selected_story_marker = UIImageView.alloc.initWithFrame(CGRectMake(CGRectGetMidX(view.bounds)- 0.05 *  view.frame.size.width,
+                                                                          view.frame.size.height - 0.05 * view.frame.size.width,
+                                                                          0.1 * view.frame.size.width, 0.05 * view.frame.size.width))
       @selected_story_marker.image = UIImage.imageNamed("Marker.png")
       hide_marker
-      append(@selected_story_marker) unless @selected_story_marker.nil?
+      view.addSubview(@selected_story_marker) unless @selected_story_marker.nil?
 
+      addSubview(view)
     end
   end
 
@@ -104,6 +93,10 @@ class MenuStoryCell < UICollectionViewCell
   # @param source [UIButton] the pressed cell's button
   def left_button_pressed (source)
     @delegate.menuStoryCell(self, leftButtonPressed: source) if @delegate.respond_to? 'menuStoryCell:leftButtonPressed:'
+  end
+
+  def cell_pressed (sender)
+    right_button_pressed(@right_button) if @right_button
   end
 
 end
