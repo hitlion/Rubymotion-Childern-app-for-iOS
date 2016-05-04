ffb91a9a6626115c482290838cac53092bf13baa
Robert Schertling <Robertschertling@googlemail.com>
2016-05-04 16:32:26 +0200

    # fixed Problems in MenuStoryCell

--- app/views/cells/menu_story_cell.rb
+++ app/views/cells/menu_story_cell.rb
@@ -9,7 +9,7 @@ class MenuStoryCell < UICollectionViewCell
       self.backgroundColor = rmq.color.babbo_orange
 
       view = UIButton.alloc.initWithFrame(self.bounds)
-      view.addTarget(self, action: "cell_pressed:", forControlEvents: UIControlEventTouchUpInside)
+      view.addTarget(self, action: 'cell_pressed:', forControlEvents: UIControlEventTouchUpInside)
       view.backgroundColor = UIColor.clearColor
 
       @background_image = UIImageView.alloc.initWithFrame(view.bounds)
@@ -17,7 +17,7 @@ class MenuStoryCell < UICollectionViewCell
       view.addSubview(@background_image) unless @background_image.nil?
 
       layer = UIImageView.alloc.initWithFrame(CGRectMake(0, 0, 0.75 * view.frame.size.width, 0.33 * view.frame.size.height ))
-      layer.image = rmq.image.resource("cells/cell_layer.png")
+      layer.image = rmq.image.resource('cells/cell_layer.png')
       view.addSubview(layer) unless layer.nil?
 
       @label = UILabel.alloc.initWithFrame(CGRectMake(0, 0, layer.frame.size.width, 0.5 * layer.frame.size.height))
@@ -29,24 +29,24 @@ class MenuStoryCell < UICollectionViewCell
 
       @left_button = UIButton.alloc.initWithFrame(CGRectMake(0.025 * layer.frame.size.width,0.6 * layer.frame.size.height,
                                                             0.425 * layer.frame.size.width, 0.35 * layer.frame.size.height))
-      @left_button.setBackgroundImage(rmq.image.resource("button_grey.png"), forState:UIControlStateNormal)
-      @left_button.setTitle("Mehr", forState: UIControlStateNormal)
-      @left_button.addTarget(self, action: "left_button_pressed:", forControlEvents: UIControlEventTouchUpInside)
+      @left_button.setBackgroundImage(rmq.image.resource('button_grey.png'), forState:UIControlStateNormal)
+      @left_button.setTitle('Mehr', forState: UIControlStateNormal)
+      @left_button.addTarget(self, action: 'left_button_pressed:', forControlEvents: UIControlEventTouchUpInside)
       device.ipad? ? @left_button.font = rmq.font.pad_medium : @left_button.font = rmq.font.phone_medium
       view.addSubview(@left_button) unless @left_button.nil?
 
       @right_button = UIButton.alloc.initWithFrame(CGRectMake(0.50 * layer.frame.size.width,0.6 * layer.frame.size.height,
                                                              0.425 * layer.frame.size.width, 0.35 * layer.frame.size.height))
-      @right_button.setBackgroundImage(rmq.image.resource("button_orange.png"), forState:UIControlStateNormal)
-      @right_button.setTitle("Öffnen", forState: UIControlStateNormal)
-      @right_button.addTarget(self, action: "right_button_pressed:", forControlEvents: UIControlEventTouchUpInside)
+      @right_button.setBackgroundImage(rmq.image.resource('button_orange.png'), forState:UIControlStateNormal)
+      @right_button.setTitle('Öffnen', forState: UIControlStateNormal)
+      @right_button.addTarget(self, action: 'right_button_pressed:', forControlEvents: UIControlEventTouchUpInside)
       device.ipad? ? @right_button.font = rmq.font.pad_medium : @right_button.font = rmq.font.phone_medium
       view.addSubview(@right_button) unless @right_button.nil?
 
       @selected_story_marker = UIImageView.alloc.initWithFrame(CGRectMake(CGRectGetMidX(view.bounds)- 0.05 *  view.frame.size.width,
                                                                           view.frame.size.height - 0.05 * view.frame.size.width,
                                                                           0.1 * view.frame.size.width, 0.05 * view.frame.size.width))
-      @selected_story_marker.image = UIImage.imageNamed("Marker.png")
+      @selected_story_marker.image = UIImage.imageNamed('Marker.png')
       hide_marker
       view.addSubview(@selected_story_marker) unless @selected_story_marker.nil?
 
